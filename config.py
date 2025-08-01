a60f4b98671f882fc7fd344b8ceeffba2c1c29ba
kiritokaneki13 <147012289+kiritokaneki13@users.noreply.github.com>
2025-08-01 00:44:47 -0600

    back3

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, db
 import os
 
 print(os.getcwd())
-cred = credentials.Certificate('c:\\Users\\prend\\Desktop\\CSSC_Antenas\\CSSC_Antenas\\cssc-antenas-firebase-adminsdk-fbsvc-6540f6b782.json')
+cred = credentials.Certificate('c:\\Users\\prend\\Desktop\\CSSC_Antenas\\CSSC_Antenas\\cssc-antenas-firebase-adminsdk-fbsvc-699491abe5.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://cssc-antenas-default-rtdb.firebaseio.com/'
 })
